c1635ca5fb9afd59be5b02b1e7141166d2bda79c
qazsato <qazsato@gmail.com>
2025-07-08 08:54:52 +0900

    fix swagger $ref resolve error

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,7 @@
-export const UI = ['stoplight', 'swagger', 'redoc']
+export const UI = {
+  STOPLIGHT: 'stoplight',
+  SWAGGER: 'swagger',
+  REDOC: 'redoc',
+}
 export const THEME = ['light', 'dark']
 export const ENABLE_INPUT_EXTS = ['.json', '.yaml', '.yml']
